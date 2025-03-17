6e7c9d455655c4d68219be4e864e965740abf61d
jennifer <cxxyao2@gmail.com>
2025-03-17 10:55:51 -0400

    add api

---
+++ book-network-api/src/test/java/com/jane/booknetworkapi/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jane.booknetworkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
