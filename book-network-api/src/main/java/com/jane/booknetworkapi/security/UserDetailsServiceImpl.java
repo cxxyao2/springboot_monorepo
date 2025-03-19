7d1dc8a2cde719da8a7e20fb9f4e8ccd37b0f494
jennifer <cxxyao2@gmail.com>
2025-03-18 22:40:28 -0400

    crafted application.yml

---
+++ book-network-api/src/main/java/com/jane/booknetworkapi/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.jane.booknetworkapi.security;
+
+import com.jane.booknetworkapi.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException(userEmail + " not found" ));
+    }
+}
